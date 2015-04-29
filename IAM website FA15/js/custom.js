7eafef2b509951b34b7b64357bd4563de0e29a6e
danhee1012 <christopher.danhee.choi@gmail.com>
2015-04-29 17:33:48 -0500

    nav jquery

--- IAM website FA15/js/custom.js
+++ IAM website FA15/js/custom.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 	$('.nav').hover(
 		function() {
-		$(this).parent("body").removeClass("collapsed");
+			$(this).parent("body").removeClass("collapsed");
+			$(this).siblings('.content').css("margin-left","200px")
 		},
 		function() {
-		$(this).parent("body").addClass("collapsed");
+			$(this).parent("body").addClass("collapsed");
+			$(this).siblings('.content').css("margin-left","50px")
 		}
 	);
 });
